4061e4fe0039961238d0525b4810260ead2471f8
Tudakshana <tudakshanamjayawardhana@gmail.com>
2026-02-22 14:05:39 +0530

    feat: add web server functionality

---
+++ web-server.js
@@ -0,0 +1,10 @@
+// Node.js Web Server Example
+const http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello World!');
+    res.end();
+}).listen(8080);
+
+console.log('Server is running on http://localhost:8080');
